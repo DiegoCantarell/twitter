0aa06c581483a1fe1334488de5bbb2e7b3295582
kantrax <diegogccantarell@gmail.com>
2022-06-09 16:44:52 -0500

    20 Adding test for Requirement 2: Unit Test - > Get all user data in a list

--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -1,6 +1,7 @@
 const UserService = require('./../../app/services/UserService')
 
 describe('Test for UserService', () => {
+	//Requirement 1
 	test('1. Create a new user using the UserService', () => {
 		const user = UserService.create(1,"diegocantarell","Diego")
 		expect(user.username).toBe("diegocantarell")
@@ -8,4 +9,14 @@ describe('Test for UserService', () => {
 		expect(user.id).toBe(1)
 		expect(user.bio).not.toBeUndefined()
 	})
+	//Requirement 2
+	        test('2. Get all user data in a list', () => {
+                const user = UserService.create(1,"diegocantarell","Diego")
+		const userInfoInList = UserService.getInfo(user)
+                expect(userInfoInList[0]).toBe(1)
+                expect(userInfoInList[1]).toBe("diegocantarell")
+                expect(userInfoInList[2]).toBe("Diego")
+                expect(userInfoInList[3]).toBe("Sin bio")
+        })
+
 })
