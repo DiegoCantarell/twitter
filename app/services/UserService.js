362c57f542528b487c49e6de056bb871e95e98c0
kantrax <diegogccantarell@gmail.com>
2022-06-09 16:46:23 -0500

    21 Updating 'UserService.js ' to add the static method for the Requirement 2 and test 2

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -4,5 +4,11 @@ class UserService{
 	static create(id,username,name){
 		return new User(id, username, name, "Sin bio")
 	}
+
+	static  getInfo(User){
+		let userInfoInList = [ User.id, User.username, User.name, "Sin bio" ]
+		return userInfoInList
+	}
+
 }
 module.exports = UserService
