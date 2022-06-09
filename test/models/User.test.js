2eee11e97d1d91be545933f0ec6ab817d5e0d6e5
DiegoCantarell <diegogccantarell@gmail.com>
2022-06-08 21:48:49 -0500

    13 Adding to the test the get test

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -8,11 +8,19 @@ describe("Unit Test for User Class", () => {
 		//Here we evaluate and validate the results of the this code
 		//This is a comparison that is going to evaluate/equalize the left value with de right value (expected value)
 		expect(user.id).toBe(1)
-		expect(user.userName).toBe("diegocantarell")
+		expect(user.username).toBe("diegocantarell")
 		expect(user.name).toBe("Diego")
 		expect(user.bio).toBe("Bio")
 		//Check if the value is not undefined in the field date
 		expect(user.dateCreated).not.toBeUndefined()
 		expect(user.lastUpdated).not.toBeUndefined()
 	});
+	test('Validate Getters', () =>{
+		const user = new User(1,"diegocantarell","Diego","Bio")
+		expect(user.getUsername).toBe("diegocantarell")
+		expect(user.getBio).toBe("Bio")
+		expect(user.getDateCreated).not.toBeUndefined()
+		expect(user.getLastUpdated).not.toBeUndefined()
+	});
+
 })
