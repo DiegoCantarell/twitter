348715b6c5146f7ad52f30cf44990c01bee5ca74
DiegoCantarell <diegogccantarell@gmail.com>
2022-06-08 21:53:26 -0500

    14 Adding getters to the class user

--- app/models/User.js
+++ app/models/User.js
@@ -1,11 +1,23 @@
 class User{
-	constructor(id,userName,name,bio,dateCreated,lastUpdated){
+	constructor(id,username,name,bio,dateCreated,lastUpdated){
 		this.id = id
-		this.userName = userName
+		this.username = username
 		this.name = name
 		this.bio = bio
 		this.dateCreated = new Date()
 		this.lastUpdated = new Date()
+	}
+	get getUsername(){
+		return this.username
+	}
+	get getBio(){
+		return this.bio
+	}
+	get getDateCreated(){
+		return this.dateCreated
+	}
+	get getLastUpdated(){
+		return this.lastUpdated
 	}
 }
 module.exports = User
