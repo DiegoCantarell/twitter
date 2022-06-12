d1a7635b1b3c6c89f6c92c0b132415124fbb319c
kantrax <diegogccantarell@gmail.com>
2022-06-12 01:14:02 -0500

    28 Adding code to app/view/UserView.js to pass the test and Sprint 3 / Requirement 1

--- app/views/UserView.js
+++ app/views/UserView.js
@@ -1,6 +1,11 @@
-const Views  = require ('./../models/User')
-const UserView{
+//const Views  = require ('./../services/UserService')
+const UserService = require('./../services/UserService')
+class UserView{
+	static createUser(payload){
+		if(payload === null)
+    		return UserService.create(1,"diegocantarell","Diego","payload no existe")		
 
+	} 
 
 }
 module.exports = UserView
