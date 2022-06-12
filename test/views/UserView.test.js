35ca7f80772ea0f757f28ae04a7e842e2196425b
kantrax <diegogccantarell@gmail.com>
2022-06-12 18:58:29 -0500

    34 Adding second test for Sprint 3 - Requirement 2 ->  'Return an error object when try to create a new user with a payload width invalid properties'

--- test/views/UserView.test.js
+++ test/views/UserView.test.js
@@ -1,11 +1,16 @@
 const UserView = require('./../../app/views/UserView')
 
 describe("Test for UserView ", () => {
-	test('Return an error object when try to create a new user with an null payload',() => {
+	test('1.Return an error object when try to create a new user with an null payload',() => {
 		const payload = null
 		const result = UserView.createUser(payload)
 		//https://jest.io/docs/using-matchers#strings
 	expect(result.error).toMatch(/payload no existe/)
 	})
-
+    test('2.Return an error object when try to create a new user with a payload width invalid properties',() => {
+        const payload = {username: null, name:12, id: "id" }         
+        const result = UserView.createUser(payload)
+        //https://jest.io/docs/using-matchers#strings
+        expect(result.error).toMatch("necesitan tener un valor valido")
+     })        
 })
