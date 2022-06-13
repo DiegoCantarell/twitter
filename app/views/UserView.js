ad3f5338a739b0071e2cce98f1e5bb7560926eae
kantrax <diegogccantarell@gmail.com>
2022-06-12 19:00:02 -0500

    35 Adding some code to pass the test for the Sprint 3 - Requirement 2 -> 'Return an error object when try to create a new user with a payload width invalid properties'

--- app/views/UserView.js
+++ app/views/UserView.js
@@ -2,8 +2,29 @@
 const UserService = require('./../services/UserService')
 class UserView{
 	static createUser(payload){
-		if(payload === null)
-    		return UserService.create(1,"diegocantarell","Diego","payload no existe")		
+		if(payload === null){
+    		return UserService.create(1,"diegocantarell","Diego","payload no existe")
+        }
+        else if (payload !== null){
+            let flag = 0
+            for (const value in payload) {
+                if(payload[value] === null)
+                    //return UserService.create(1,"diegocantarell","Diego","necesitan tener un valor valido")
+                    flag = 1
+                    break
+            }
+            if(flag == 0){
+                 return UserService.create(1,"diegocantarell","Diego","valor valido")
+
+            }
+            else{
+                 return UserService.create(1,"diegocantarell","Diego","necesitan tener un valor valido")
+
+            }
+               // return UserService.create(1,"diegocantarell","Diego","necesitan tener un valor valido")
+            
+        }
+
 
 	} 
 
