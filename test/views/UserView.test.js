ed06d0663aaeb8fba268a012bdb484800360cee4
kantrax <diegogccantarell@gmail.com>
2022-06-12 19:16:11 -0500

    36 Adding test for Sprint 3 - Requirement 3 -> Return an error object when try to create a new user with a payload with missing properties

--- test/views/UserView.test.js
+++ test/views/UserView.test.js
@@ -12,5 +12,11 @@ describe("Test for UserView ", () => {
         const result = UserView.createUser(payload)
         //https://jest.io/docs/using-matchers#strings
         expect(result.error).toMatch("necesitan tener un valor valido")
-     })        
+     })
+    test('3.Return an error object when try to create a new user with a payload with missing properties',() => {
+        const payload = {username: "username" }
+        const result = UserView.createUser(payload)
+        //https://jest.io/docs/using-matchers#strings
+        expect(result.error).toMatch("necesitan tener un valor valido")
+    })
 })
