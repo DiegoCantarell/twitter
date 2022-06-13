e1c3e0e2943a5faff4a7e80faf9c370b190ebfc8
kantrax <diegogccantarell@gmail.com>
2022-06-12 21:38:51 -0500

    38 Adding test for Sprint 3 - Requirement 4 -> Create a user by a given valid payload

--- test/views/UserView.test.js
+++ test/views/UserView.test.js
@@ -19,4 +19,12 @@ describe("Test for UserView ", () => {
         //https://jest.io/docs/using-matchers#strings
         expect(result.error).toMatch("necesitan tener un valor valido")
     })
+    test('3.Create a user by a given valid payload',() => {
+        const payload = {username: "username", id: 1, name: "name" }
+        const result = UserView.createUser(payload)
+        expect(result.name).toBe("name")
+        expect(result.username).toBe("username")
+        expect(result.id).toBe(1)
+    })
+
 })
